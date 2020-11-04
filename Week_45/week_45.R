281dc7df2f20a16e9341c758d740ef1d8d63e65f
Kevin Magnan <kjmagnan1s@gmail.com>
2020-11-03 22:46:55 -0600

    new files

---
+++ Week_45/week_45.R
@@ -0,0 +1,31 @@
+pacman::p_load(
+  tidyverse,
+  tidytuesdayR,
+  plotly
+)
+
+ikea <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-11-03/ikea.csv')
+
+ggplotly(
+  ikea %>%
+    mutate(price_usd = price * 0.27) %>%
+    select(item_id, name, category, price_usd) %>% 
+    group_by(category, name) %>% 
+    summarise(name_count = n(), 
+              price_per_unit = round(mean(price_usd), 2)) %>%
+    ggplot(aes(x = name_count, y = price_per_unit, text = paste(
+      "Ikea Category:", category,
+      "<br>", "Number of items in", category,"category:", name_count, name,
+      "<br>", "Price per unit: $", price_per_unit
+    ))) +
+    geom_point(aes(size = price_per_unit, color = category)) +
+    theme(legend.title = element_blank()) +
+    labs(title = "TidyTuesday Week 45: Ikea Furniture | Made by @KevinMagnan",
+         x = "Count of Ikea Furniture By Name",
+         y = "Price Per Unit"
+    ), tooltip = "text"
+) %>% 
+  layout(annotations = list(x = 40,
+                            y = 1200,
+                            showarrow = F,
+                            text = "Hover for more details!"))
